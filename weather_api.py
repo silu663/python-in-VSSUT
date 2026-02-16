b7d61a0bd94a08f51056c6fc4d1ce6df7a7fd4b1
Ninja@07 <siludas978@gmail.com>
2026-02-16 11:50:22 +0530

    changing the file name

--- weather_api.py
+++ weather_api.py
@@ -4,7 +4,7 @@ apiKey = "ca48f74c00fc1ba1e6f86999e6462258"
 apiUrl = f'https://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${apiKey}&units=metric'
 
 response = requests.get(apiUrl)
-data = response.json()  
+data = response.json()       
 print(data)
 
 
