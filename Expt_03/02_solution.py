cb7eb0728d5d47a6c292b3352654eb23a26446ba
Ninja@07 <siludas978@gmail.com>
2026-02-16 12:02:11 +0530

    small changes in the file

--- Expt_03/02_solution.py
+++ Expt_03/02_solution.py
@@ -7,15 +7,6 @@ print(float(5))
 # str(x) - Convert to string
 print(str(123))         
 
-# tuple(x) - Convert to tuple
-print(tuple([1, 2, 3]))  
-
-# list(x) - Convert to list
-print(list((4, 5, 6)))  
-
-# set(x) - Convert to set
-print(set([1, 2, 2, 3])) 
-
 # ord(x) - Character to integer (ASCII/Unicode code point)
 print(ord('A'))         
 
@@ -28,5 +19,3 @@ print(oct(10))
 # hex(x) - Integer to hexadecimal string
 print(hex(255))          
 
-# dict(x) - Create dictionary from key-value pairs
-print(dict([('a', 1), ('b', 2)]))  
